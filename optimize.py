aa5e43d53777b593bcff4ef7b2142bb1c881cd32
Michael Behrisch <oss@behrisch.de>
2021-05-16 13:21:21 +0200

    better iterative optimization

--- optimize.py
+++ optimize.py
@@ -4,10 +4,11 @@ import argparse
 import shutil
 import subprocess
 import atexit
+import json
 
 #from optimparallel import minimize_parallel
 
-def run_evaluation(par, names, agent):
+def start_evaluation(par, names, agent, simcfg):
     if agent.endswith("/"):
         agent = agent[:-1]
     par_agent = agent + "_".join(["_".join(p) for p in zip(names, [str(pp) for pp in par])])
@@ -24,20 +25,30 @@ def run_evaluation(par, names, agent):
                     line = " ".join(ls) + "\n"
                     break
             cfg.write(line)
-    with open(os.path.join("cfg", "simulator.cfg")) as cfg_in, open(os.path.join(par_agent, "simulator.cfg"), "w") as cfg:
+    with open(simcfg) as cfg_in, open(os.path.join(par_agent, "simulator.cfg"), "w") as cfg:
         for line in cfg_in:
             ls = line.split()
             if ls and ls[0] == 'report_log_addr':
                 ls[2] = "./%s/" % par_agent
                 line = " ".join(ls) + "\n"
             cfg.write(line)
-    return subprocess.Popen("docker run -v $PWD:/starter-kit kdd /starter-kit/run.sh %s" % par_agent, shell=True)
+    return subprocess.Popen("docker run -v $PWD:/starter-kit kdd /starter-kit/run.sh %s" % par_agent, shell=True), par_agent, par
+
+def get_score(par_agent):
+    scores = json.load(open(os.path.join(par_agent, "scores.json")))
+    return scores["data"]["delay_index"]
+
+def run_evaluation(par, names, agent):
+    p, par_agent, _ = start_evaluation(par, names, agent)
+    p.wait()
+    return get_score(par_agent)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("agent")
     parser.add_argument("-t", "--threads", type=int)
     parser.add_argument("-s", "--steps", type=int, default=10)
+    parser.add_argument("-c", "--simulator-cfg", default="cfg/simulator.cfg")
     args = parser.parse_args()
 
     init = []
@@ -57,18 +68,26 @@ if __name__ == "__main__":
             ranges.append(r)
             names.append(ls[0])
     print("optimizing", names, init, ranges)
-    atexit.register(lambda: subprocess.call("docker kill $(docker ps -q)", shell=True))
+    atexit.register(lambda: subprocess.call("docker kill $(docker ps -q)", shell=True) if procs else None)
+    values = list(init)
     for idx in range(len(init)):
+        scores = {}
         step = (ranges[idx][1] - ranges[idx][0]) / args.steps
-        values = list(init)
         procs = []
         for i in range(args.steps):
             values[idx] = ranges[idx][0] + i * step
-            procs.append(run_evaluation(values, names, args.agent))
+            procs.append(start_evaluation(values, names, args.agent, args.simulator_cfg))
             if args.threads and len(procs) == args.threads:
-                for p in procs:
-                    p.wait()
+                for proc, a, par in procs:
+                    proc.wait()
+                    scores[a] = (par, get_score(a))
                 procs = []
-        for p in procs:
-            p.wait()
+        for proc, a, par in procs:
+            proc.wait()
+            scores[a] = (par, get_score(a))
+        procs = []
+        min_agent, (min_par, min_score) = min(scores.items(), key=lambda i: i[1][1])
+        print("scores", scores)
+        print("min", min_agent, min_par, min_score)
+        values[idx] = min_par[idx]
 #    opt = minimize_parallel(run_evaluation, init, names, bounds=ranges)
