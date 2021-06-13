d6b80b9823053277128cc825f51c220346fe2666
Michael Behrisch <oss@behrisch.de>
2021-06-13 08:34:09 +0200

    adapting some changes from merge

--- evaluate.py
+++ evaluate.py
@@ -461,7 +461,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--metric_period",
         help="period of scoring",
-        default=3600,
+        default=200,
         type=int
     )
     parser.add_argument(
