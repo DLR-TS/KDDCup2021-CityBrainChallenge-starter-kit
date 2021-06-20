83982b98dfed0866e0b9995cb78adc298a854bf2
Michael Behrisch <michael.behrisch@dlr.de>
2021-06-20 11:24:52 +0200

    parameter change seems to be more important than slicing

--- qlagent/gym_cfg.py
+++ qlagent/gym_cfg.py
@@ -6,18 +6,18 @@ class gym_cfg():
             'custom_observation' : True
         }
 
-SWITCH_THRESH = [0.8, 0.5, 0.4] #   difference in queue lengths which triggers switch optimized -0.1 0.9
-ROUTE_LENGTH_WEIGHT = [689.0, 690, 690.0] #   to be optimized 500 900#
-MIN_CHECK_LENGTH = [18.0, 26, 26.0] #    look upstream to find more queued vehicles if a lane is shorter than this optimized 10 50
+SWITCH_THRESH = [0.8, 0.8, 0.4] #   difference in queue lengths which triggers switch optimized -0.1 0.9
+ROUTE_LENGTH_WEIGHT = [689.0, 689, 690.0] #   to be optimized 500 900#
+MIN_CHECK_LENGTH = [18.0, 18, 26.0] #    look upstream to find more queued vehicles if a lane is shorter than this optimized 10 50
 JAM_THRESH = [0.91, 0.91, 0.91] #   at which relative occupancy a lane is considered jammed, to be optimized 0.9 0.95#
-HEADWAY = [2.1, 1.5, 1.5] #    to be optimized 1.2 2.2
-SLOW_THRESH = [0.07, 0.1, 0.09999999999999999] #   at which relative speed a vehicle is considered slow, to be optimized 0.05 0.15#
-SPEED_THRESH = [0.02, 0.05, 0.08] #   at which relative speed a lane is considered jammed, to be optimized 0.01 0.11#
-JAM_BONUS = [8.0, 5, 5.0] #   bonus vehicles to add to a jammed lane per act call (every 10s) until it gets green optimized 1 11#
+HEADWAY = [2.1, 2.1, 1.5] #    to be optimized 1.2 2.2
+SLOW_THRESH = [0.07, 0.07, 0.09999999999999999] #   at which relative speed a vehicle is considered slow, to be optimized 0.05 0.15#
+SPEED_THRESH = [0.02, 0.02, 0.08] #   at which relative speed a lane is considered jammed, to be optimized 0.01 0.11#
+JAM_BONUS = [8.0, 8, 5.0] #   bonus vehicles to add to a jammed lane per act call (every 10s) until it gets green optimized 1 11#
 MAX_GREEN_SEC = [156.0, 156, 156.0] #   to be optimized 140 220#
-PREFER_DUAL_THRESHOLD = [4.0, 3, 8.0] #    How many vehicles could pass the intersection in 5s all red (if we have more vehicles in a dual queue switching, beats single queue discharge) optimized 1 11
+PREFER_DUAL_THRESHOLD = [4.0, 4, 8.0] #    How many vehicles could pass the intersection in 5s all red (if we have more vehicles in a dual queue switching, beats single queue discharge) optimized 1 11
 STOP_LINE_HEADWAY = [9.6, 9.6, 10.4] #   seconds to the stopline to be included in the queue optimized 8 12#
-BUFFER_THRESH = [1.0, 5, 2.0] #    number of vehicles in lane insertion buffer to consider lane jammed optimized 1 11
-FUTURE_JAM_LOOKAHEAD = [9, 7, 7.0] #  number of edges optimized 0 10#
+BUFFER_THRESH = [1.0, 1, 2.0] #    number of vehicles in lane insertion buffer to consider lane jammed optimized 1 11
+FUTURE_JAM_LOOKAHEAD = [9, 9, 7.0] #  number of edges optimized 0 10#
 SATURATED_THRESHOLD = [44, 44, 44.0] #  optimized 30 50#
-SATURATION_INC = [5, 10, 8.0] #  optimized 5 15#
+SATURATION_INC = [5, 5, 8.0] #  optimized 5 15#
